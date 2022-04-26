081786d627c6db4fe75867b9538d19379c11ea54
Bugra Ozturk <oozturkks@gmail.com>
2022-04-26 03:04:13 +0300

    SQLServer Connected

--- Models/Data/wowContext.cs
+++ Models/Data/wowContext.cs
@@ -5,9 +5,19 @@ namespace wowAPI.Data
 {
     public class DataContext : DbContext
     {
-    public DbSet <MajorCharacter> MajorCharacters { get; set; }
-    public DbSet <Expansion> Expansions { get; set; }
-    public DbSet <Race> Races { get; set; }
-    public DbSet <Class> Classes { get; set; }
+        public DataContext (DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+        
+        public DbSet <MajorCharacter> MajorCharacters { get; set; }
+        public DbSet <Expansion> Expansions { get; set; }
+        public DbSet <Race> Races { get; set; }
+        public DbSet <Class> Classes { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using wowAPI.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Configuration.AddJsonFile("appsettings.json");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -23,3 +27,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
