ed3956aed12b14ef12e97b252c78655d727d92f3
Bugra Ozturk <oozturkks@gmail.com>
2022-04-29 15:07:36 +0300

    Typo

--- Models/Entities/MajorCharacter.cs
+++ Models/Entities/MajorCharacter.cs
@@ -6,29 +6,17 @@ namespace wowAPI.Entities
     public class MajorCharacter : BaseEntity
     {
         public string Name { get; set;}
-
         public Gender Gender { get; set;}
-
         public int Level { get; set;}
-
         [ForeignKey("ClassId")]
-        
         public int ClassId { get; set; }
-        
         public virtual Class Class { get; set; }
-
         [ForeignKey("ExpansionId")]
-        
+
         public virtual Expansion Expansion {get; set;}
-
         [ForeignKey("Race")]
-
         public int RaceId { get; set; }
-        
         public virtual Race Race { get; set; }
-        
         public Faction Faction {get; set;}
-
-        
     }
 }
