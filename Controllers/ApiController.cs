7d85837d75b58825530c057bdac0c8647e03d11a
Bugra Ozturk <oozturkks@gmail.com>
2022-05-17 02:49:57 +0300

    HTTP GET-POST-DELETE added

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using wowAPI.Data;
 using wowAPI.Entities;
+using Newtonsoft.Json;
 
 namespace wowAPI.Controllers
 {
@@ -19,25 +20,48 @@ namespace wowAPI.Controllers
         [HttpGet("/api/character")]
         public async Task<string> GetCharacters()
         {
-            return "";
+            var characterData = _db.MajorCharacters.ToList();
+            return JsonConvert.SerializeObject(characterData);
         }
 
         [HttpGet("/api/character/{characterId}")]
         public async Task<string> GetCharacterById(int characterId)
         {
-            return "";
+        var character = await _db.MajorCharacters.FindAsync(characterId);
+        if (character == null)
+        {
+            return "Character Not Found !";
+        }
+            return JsonConvert.SerializeObject(character);
         }
 
         [HttpPost("/api/character/{characterId}")]
         public async Task<string> UpdateCharacterById(int characterId, [FromBody] MajorCharacter newModel)
         {
-            return "";
+            _db.Add(newModel);
+            await _db.SaveChangesAsync();
+            return "true";
+        }
+        
+        [HttpPost("/api/expansion")]
+        public async Task<string> AddExpansion(int expansionId, [FromBody] Expansion newModel)
+        {
+            _db.Expansions.Add(newModel);
+            await _db.SaveChangesAsync();
+            return "true";
         }
 
         [HttpDelete("/api/character/{characterId}")]
         public async Task<string> DeleteCharacterById(int characterId)
         {
-            return "";
+        var character = await _db.MajorCharacters.FindAsync(characterId);
+        if (character == null)
+        {
+            return "Character Not Found !";
+        }
+            _db.MajorCharacters.Remove(character);
+            _db.SaveChanges();
+            return "Character Deleted";
         }
     }
 }
